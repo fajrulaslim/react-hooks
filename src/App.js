72a9b5b4e7990244154d3d18fdc49dcdf3ecebb6
Fajrul Aslim <30421357+fajrulaslim@users.noreply.github.com>
2021-12-13 15:26:59 +0700

    tutorial react hooks

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css';
+import ComponentC from './components/ComponentC';
+
+
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
+
+function App() {
+  return (
+    <div className="App">
+      <UserContext.Provider value={'Aslim'}>
+        <ChannelContext.Provider value={'Sedang Ngoding'}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
+    </div>
+  );
+}
+
+export default App;
